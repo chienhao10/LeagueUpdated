e8cf97d708d3991ffe54378a199ee0b5cc1456fb
LordZEDith <vrandan@gmail.com>
2016-10-05 12:19:33 -0700

    Updated to 6.20(Can't Test L# Down :( But should work )

---
+++ Avoid/Program.cs
@@ -0,0 +1,17 @@
+﻿using LeagueSharp;
+using LeagueSharp.Common;
+
+namespace Avoid
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+#if DEBUG
+            Utils.ClearConsole();
+#endif
+            CustomEvents.Game.OnGameLoad += uselessArgs => Avoid.OnGameStart();
+            Game.PrintChat("<font size='30'>Avoid</font> <font color='#b756c5'>by LordZEDith</font>");
+        }
+    }
+}
