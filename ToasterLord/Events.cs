32e35af7bab322ce9524ddbdce257536f37f96e4
LordZEDith <LordZEDith@users.noreply.github.com>
2016-11-30 22:02:17 -0700

    First

---
+++ ToasterLord/Events.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using LeagueSharp;
+
+namespace ToasterLord
+{
+    class Events
+    {
+        public class Game
+        {
+            public delegate void OnGameStartet(EventArgs args);
+
+            static Game()
+            {
+                LeagueSharp.Game.OnProcessPacket += OnGameProcessPacket;
+            }
+
+            public static event OnGameStartet OnGameStart;
+
+            private static void OnGameProcessPacket(GamePacketEventArgs args)
+            {
+                if (LeagueSharp.Game.Mode != GameMode.Running || OnGameStart == null)
+                    return;
+                OnGameStart(new EventArgs());
+                LeagueSharp.Game.OnProcessPacket -= OnGameProcessPacket;
+            }
+        }
+    }
+}
